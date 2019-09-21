a17cd0ba5749776dd40bd2c3906b3f7669082046
Aldhair Vera <aldhairvera15@gmail.com>
2019-09-20 23:19:33 -0500

    Optimize listeners / media queries JS

--- index.js
+++ index.js
@@ -2,31 +2,35 @@ const interaction=document.querySelector('.header-menu');
 const button=document.querySelector('.burger-button');
 const header_links=document.querySelectorAll('.header-link');
 
-button.addEventListener('click',isActive);
-// for(var i=0;i<header_links;i++)
-// {
-//     header_links[i].setAttribute('href','javascript:takeOff()');
-// }
-
-// console.log(header_links);
-
-done=false;
 
 function isActive(){
-    if(!done)
+    if(!interaction.classList.contains('is-active'))
     {
         interaction.classList.add('is-active');
-        done=true;
     }
-    else if(done){
+    else if(interaction.classList.contains('is-active')){
         interaction.classList.remove('is-active');
-        done=false;
     }
 }
 
 function takeOff(){
-    setTimeout(() => {
-        interaction.classList.remove('is-active');
-        done=false;
-    }, 50);
+    if(interaction.classList.contains('is-active'))
+    {
+        setTimeout(() => {
+            interaction.classList.remove('is-active');
+        }, 50);
+    }
+}
+
+const ipad=window.matchMedia('screen and (max-width:1024px) and (min-width:769px)');
+
+ipad.addListener(validation);
+
+function validation(event){
+    if(event.matches){
+        button.addEventListener('click',isActive);
+    }
+    else{
+        button.removeEventListener('click',isActive);
+    }
 }
